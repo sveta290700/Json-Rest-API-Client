66e1e719a34e88e9e5532d342802cba0857e3dbe
sveta290700 <777rsb@mail.ru>
2021-10-16 17:35:20 +1000

    Gitignore update

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpClient\CurlHttpClient;
 
 require_once 'vendor/autoload.php';
 
-$client = new ApiClient(new CurlHttpClient(), 'http://178.62.205.95', 'sveta5', '123456');
+$client = new ApiClient(new CurlHttpClient(), 'http://178.62.205.95', 'svet', '123456');
 echo $client->getTodos()->getMessage()."\n";
 echo $client->addTodo("new todo")->getMessage()."\n";
 echo $client->updateTodo("updated todo", 1)->getMessage()."\n";
